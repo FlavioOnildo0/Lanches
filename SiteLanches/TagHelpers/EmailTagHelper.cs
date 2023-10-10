7706fc1c5c1dabf60977d16c6eacf2ac61894da3
User <User@USUARIO>
2023-10-09 22:14:11 -0400

    Criação da TagHelps parte 1

---
+++ SiteLanches/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SiteLanches.TagHelprs
+{
+    public class EmailTagHelper:TagHelper
+    {
+        public string Endereco { get; set; }
+        public string Conteudo { get; set; }
+
+        //metodo
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName= "a";//usando o a porque será usado um link
+            output.Attributes.SetAttribute("href", "Flavio:" + Endereco);
+            output.Content.SetContent(Conteudo);
+        }
+    }
+}
